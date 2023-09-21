1d5261360f89c0d1546022853b12d881df79355f
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-21 19:22:47 +0530

    added faker data

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,11 +1,23 @@
 import React, { createContext } from 'react'
+import { faker } from '@faker-js/faker';
 
 const Cart = createContext(null);
 
 const Context = ({children}) => {
+    
+    const products = [...Array(20)].map(() => ({
+        id: faker.string.uuid(),
+        name: faker.commerce.productName(),
+        price: faker.commerce.price(),
+        image: faker.image.url,
+        inStock: faker.helpers.arrayElement([0,3,5,6,7]),
+        fastDelivery: faker.datatype.boolean(),
+        ratings: faker.helpers.arrayElement([1,2,3,4,5]),
+    }));
+
   return (
     <Cart.Provider>
-    {children}
+        {children}
     </Cart.Provider>
   )
 }
