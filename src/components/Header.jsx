ded9be8a5a7afccd879087e5b7f32cfb51954398
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-21 17:03:54 +0530

    built header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Container, Dropdown, FormControl, Nav, Navbar } from 'react-bootstrap';
+import {BiShoppingBag} from 'react-icons/bi';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Navbar bg="dark" variant='dark' style={{height:80}}>
+        <Container>
+            <Navbar.Brand>
+                <a href='/'>Shopping Cart</a>
+            </Navbar.Brand>
+            <Navbar.Text className='search'>
+                <FormControl 
+                    placeholder='search product...'
+                    style={{width:500}}
+                    className='m-auto'
+                />
+            </Navbar.Text>
+            <Nav>
+                <Dropdown align='end'>
+                    <Dropdown.Toggle variant="success" className='d-flex align-items-center gap-3'>
+                        <BiShoppingBag color='white' size={33} />
+                        <span className='bg-success fs-4'>0</span>
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu style={{minWidth: 370}}>
+                        <span style={{padding: 10}}>Cart is Empty!</span>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Nav>
+        </Container>
+    </Navbar>
   )
 }
 
