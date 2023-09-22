5b8f2b740defcb01e78c8ca0757dab29d881c4ad
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-22 13:45:49 +0530

    Cart Dropdown functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Container, Dropdown, FormControl, Nav, Navbar } from 'react-bootstrap';
 import {BiShoppingBag} from 'react-icons/bi';
 import { Link } from 'react-router-dom';
+import { CartState } from '../context/Context';
+import { AiFillDelete } from 'react-icons/ai';
 
 const Header = () => {
+    const {state: {cart}, dispatch} = CartState();
   return (
     <Navbar bg="dark" variant='dark' style={{height:80}}>
         <Container>
@@ -21,11 +24,42 @@ const Header = () => {
                 <Dropdown align='end'>
                     <Dropdown.Toggle variant="success" className='d-flex align-items-center gap-3'>
                         <BiShoppingBag color='white' size={26} />
-                        <span className='bg-transparent fs-5'>0</span>
+                        <span className='bg-transparent fs-5'>{cart.length}</span>
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu style={{minWidth: 370}}>
-                        <span style={{padding: 10}}>Cart is Empty!</span>
+                    {
+                        cart.length>0 ?
+                        (
+                            <Fragment>
+                                {
+                                    cart.map(product => (
+                                        <span className='cartitem' key={product.id}>
+                                            <img
+                                                src={product.image}
+                                                className="cartItemImg"
+                                                alt={product.name}
+                                            />
+                                            <div className='cartItemDetail'>
+                                                <span>{product.name}</span>
+                                                <span>₹ {product.price.split(".")[0]}</span>
+                                            </div>
+                                            <AiFillDelete
+                                                size={20}
+                                                style={{cursor: "pointer"}}
+                                                onClick={() => dispatch({
+                                                    type: "REMOVE_FROM_CART",
+                                                    payload: product,
+                                                })}
+                                            />
+                                        </span>
+                                    ))
+                                }
+                            </Fragment>
+                        ) :
+                        (<span style={{padding: 10}}>Cart is Empty!</span>)
+                    }
+                        
                     </Dropdown.Menu>
                 </Dropdown>
             </Nav>
